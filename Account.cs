7334d7dce8dfe9b3dd687d591fa91693d868d82e
Eberhard Beilharz <eb1@sil.org>
2012-02-08 08:27:08 +0100

    Update to new account numbers

--- Account.cs
+++ Account.cs
@@ -34,7 +34,9 @@ namespace TntMPDConverter
 			ProcessAccountNo();
 			if ((m_AccountNo == 7100) || (m_AccountNo == 7800) ||
 				// new account numbers starting 1/2011
-				(m_AccountNo == 1191) || (m_AccountNo == 1197) || (m_AccountNo == 1185))
+				(m_AccountNo == 1191) || (m_AccountNo == 1197) || (m_AccountNo == 1185) ||
+				// new account numbers starting 12/2011
+				(m_AccountNo == 3220) || (m_AccountNo == 3231))
 			{
 				return new ProcessingDonations(Reader);
 			}
