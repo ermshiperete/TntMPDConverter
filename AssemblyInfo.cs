8b3d7df973b0c4bd466884f6aa837c0282ef104d
Eberhard Beilharz <eb1@sil.org>
2013-01-31 15:24:53 +0100

    Update changelog.txt and version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,14 +1,14 @@
 // Assembly TntMPDConverter, Version 1.0.0.0
 
-[assembly: System.Reflection.AssemblyVersion("1.4.4.0")]
+[assembly: System.Reflection.AssemblyVersion("1.4.5.0")]
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
 [assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows = true)]
 [assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
-[assembly: System.Reflection.AssemblyFileVersion("1.4.4.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.4.5.0")]
 [assembly: System.Runtime.InteropServices.Guid("e041e1a5-ff0f-484c-9c80-918849b4e2b3")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Reflection.AssemblyTrademark("")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9  2006-2012 Eberhard Beilharz")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9  2006-2013 Eberhard Beilharz")]
 [assembly: System.Reflection.AssemblyProduct("TntMPDConverter")]
 [assembly: System.Reflection.AssemblyCompany("")]
 [assembly: System.Reflection.AssemblyConfiguration("")]
