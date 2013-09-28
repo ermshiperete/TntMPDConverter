037dd81a59773498786b8030b1cdfeab4e924a4c
Eberhard Beilharz <eb1@sil.org>
2013-09-28 19:44:17 +0200

    Switch to the cross-platform UI toolkit xwt
    This replaces the former WinForms implementation and promises better
    support on Linux platforms and a more modern look on Windows. This
    change adds the Linux part.

---
+++ ConvertDialog.cs
@@ -0,0 +1,87 @@
+// Copyright (c) 2013, Eberhard Beilharz.
+// Distributable under the terms of the MIT license (http://opensource.org/licenses/MIT).
+using System;
+using System.IO;
+using Xwt;
+using TntMPDConverter.Properties;
+
+namespace TntMPDConverter
+{
+	public class ConvertDialog: Dialog
+	{
+		private TextEntry m_SourceFile;
+		private TextEntry m_TargetDir;
+
+		public ConvertDialog()
+		{
+			this.Width = 500;
+			this.Height = 300;
+			this.Title = "TntMPD Converter";
+			var vbox = new VBox();
+			vbox.PackStart(new Label("Projektabrechnung"));
+			var hbox = new HBox();
+			m_SourceFile = new TextEntry();
+			hbox.PackStart(m_SourceFile, true, true);
+			var button = new Button("...");
+			button.Clicked += OnSourceClicked;
+			hbox.PackEnd(button);
+			vbox.PackStart(hbox);
+			vbox.PackStart(new Label("Zielverzeichnis"));
+			hbox = new HBox();
+			m_TargetDir = new TextEntry();
+			hbox.PackStart(m_TargetDir, true, true);
+			button = new Button("...");
+			button.Clicked += OnTargetClicked;
+			hbox.PackEnd(button);
+			vbox.PackStart(hbox);
+			var dialogButton = new DialogButton(Command.Ok);
+			dialogButton.Clicked += OnOkClicked;
+			Buttons.Add(dialogButton);
+			dialogButton = new DialogButton(Command.Cancel);
+			Buttons.Add(dialogButton);
+			Content = vbox;
+		}
+
+		private void OnOkClicked (object sender, EventArgs e)
+		{
+			if ((m_SourceFile.Text.Length > 0) && (m_TargetDir.Text.Length > 0))
+			{
+				Settings.Default.SourceFile = m_SourceFile.Text;
+				Settings.Default.TargetPath = m_TargetDir.Text;
+				var statement = new ConvertStatement();
+				statement.DoConversion();
+				MessageDialog.ShowMessage("Konvertierung abgeschlossen");
+				Settings.Default.Save();
+			}
+		}
+
+		private void OnSourceClicked(object sender, EventArgs e)
+		{
+			using (var dlg = new OpenFileDialog("Projektabrechnung ausw\u00e4hlen"))
+			{
+				dlg.Filters.Add(new FileDialogFilter("RTF-Dateien", "*.rtf"));
+				dlg.Filters.Add(new FileDialogFilter("Alle Dateien", "*.*"));
+				dlg.InitialFileName = m_SourceFile.Text;
+				if (dlg.Run())
+				{
+					m_SourceFile.Text = dlg.FileName;
+					if (m_TargetDir.Text.Length == 0)
+						m_TargetDir.Text = Path.GetDirectoryName(dlg.FileName);
+				}
+			}
+		}
+
+		private void OnTargetClicked(object sender, EventArgs e)
+		{
+			using (var folderBrowserDialog = new SelectFolderDialog("Zielverzeichnis ausw\u00e4hlen"))
+			{
+				folderBrowserDialog.CurrentFolder = m_TargetDir.Text.Length > 0 && m_SourceFile.Text.Length > 0 ? Path.GetDirectoryName(m_SourceFile.Text) : m_TargetDir.Text.Length > 0 ? m_TargetDir.Text : Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+				if (folderBrowserDialog.Run())
+				{
+					m_TargetDir.Text = folderBrowserDialog.CurrentFolder;
+				}
+			}
+		}
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Windows.Forms;
+using Xwt;
 using System.IO;
 using TntMPDConverter.Properties;
 
@@ -10,54 +10,35 @@ namespace TntMPDConverter
 		[STAThread]
 		private static void Main(string[] args)
 		{
-			if (args.Length < 1 || args[0] != "--nested")
+			ToolkitType tkType = ToolkitType.Wpf;
+			if (Environment.OSVersion.Platform == PlatformID.Unix)
+				tkType = ToolkitType.Gtk;
+			Application.Initialize(tkType);
+			Upgrade();
+
+			if (args.Length == 2)
 			{
-				Upgrade();
-
-				// we need to use different config file depending on OS.
-				// On Linux we want to use SilUtils.dll compiled with GTK so that we get GTK file
-				// dialogs whereas on Windows we need a different SilUtils.dll so that we don't have
-				// the dependency on GTK. See http://stackoverflow.com/a/305711 for algorithm.
-				var setup = new AppDomainSetup();
-				setup.ApplicationBase = "file://" + Environment.CurrentDirectory;
-				setup.DisallowBindingRedirects = true;
-				setup.DisallowCodeDownload = true;
-
-				if (Environment.OSVersion.Platform == PlatformID.Unix)
-					setup.ConfigurationFile = Path.Combine(Environment.CurrentDirectory, "linux.config");
-				else
-					setup.ConfigurationFile = Path.Combine(Environment.CurrentDirectory, "windows.config");
-
-				var domain = AppDomain.CreateDomain("TntMPDConvert", null, setup);
-				var newArgs = new string[args.Length + 1];
-				newArgs[0] = "--nested";
-				Array.Copy(args, 0, newArgs, 1, args.Length);
-				domain.ExecuteAssembly("TntMPDConverter.exe", null, newArgs);
-			}
-			else
-			{
-				if (args.Length == 2)
+				if (!File.Exists(args[0]))
 				{
-					if (!File.Exists(args[0]))
-					{
-						Console.WriteLine("Source file {0} doesn't exist.", args[0]);
-						return;
-					}
-					if (!Directory.Exists(args[1]))
-					{
-						Console.WriteLine("Target directory {0} doesn't exist.", args[1]);
-						return;
-					}
-					Settings.Default.SourceFile = args[0];
-					Settings.Default.TargetPath = args[1];
-					new ConvertStatement().DoConversion();
-					Settings.Default.Save();
+					Console.WriteLine("Source file {0} doesn't exist.", args[0]);
 					return;
 				}
+				if (!Directory.Exists(args[1]))
+				{
+					Console.WriteLine("Target directory {0} doesn't exist.", args[1]);
+					return;
+				}
+				Settings.Default.SourceFile = args[0];
+				Settings.Default.TargetPath = args[1];
+				new ConvertStatement().DoConversion();
+				Settings.Default.Save();
+				return;
+			}
 
-				Application.EnableVisualStyles();
-				Application.SetCompatibleTextRenderingDefault(false);
-				new Form1().ShowDialog();
+			using (var dlg = new ConvertDialog())
+			{
+				dlg.Show();
+				dlg.Run();
 			}
 		}
 
