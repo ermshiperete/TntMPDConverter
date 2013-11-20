08307565d1ba7ba8cf09bd08aab67e24e391eff0
Eberhard Beilharz <eb1@sil.org>
2013-11-20 10:00:59 +0100

    Fix unit tests

--- TntMPDConverterTests/RtfConverterTests.cs
+++ TntMPDConverterTests/RtfConverterTests.cs
@@ -88,29 +88,6 @@ namespace TntMPDConverter
 		}
 
 		[Test]
-		public void MultipleParasOfSameKindWithSlightlyDifferentTabs()
-		{
-			File.WriteAllText(m_TempFile, @"{\rtf1\ansi\ansicpg65001\deff0\deflang1033{" +
-				@"\fonttbl{\f0\fnil Arial;}}{\colortbl\red0\green0\blue0 ;}" +
-				@"{\*\generator NRtfTree Library 0.3.0;}\uc1\margl1133\margr1133\margt1133" +
-				@"\margb1133\viewkind4" +
-				@"\pard\plain\sb82\tx1362\tqr\tx2785\tqr\tx3334\tqr\tx4012\tx4310\tx4466\tx5443" +
-				@"{\plain\tab\fs14\f3\cf0\cb1 One\plain\tab\fs14\f3\cf0\cb1 Two\plain\tab\fs14\f3\cf0\cb1 Three" +
-				@"\plain\tab\fs14\f3\cf0\cb1 Four\plain\tab\fs14\f3\cf0\cb1 Five\plain\tab\fs14\f3\cf0\cb1 Six" +
-				@"\plain\tab\fs14\f3\cf0\cb1 Seven {\fs18\par}}" +
-				@"\pard\plain\tx5443{\plain\tab\fs14\f3\cf0\cb1 Eight{\fs15\par}}" +
-				@"\pard\plain\sb82\tx1362\tqr\tx2785\tqr\tx3334\tqr\tx4310\tx4466\tx5443" +
-				@"{\plain\tab\fs14\f3\cf0\cb1 1One\plain\tab\fs14\f3\cf0\cb1 2Two\plain\tab\fs14\f3\cf0\cb1 3Three" +
-				@"\plain\tab\fs14\f3\cf0\cb1 5Five\plain\tab\fs14\f3\cf0\cb1 6Six" +
-				@"\plain\tab\fs14\f3\cf0\cb1 7Seven{\fs18\par}}" +
-				@"}");
-
-			var converter = new RtfConverter(m_TempFile);
-			Assert.That(converter.Text, Is.EqualTo("\tOne\tTwo\tThree\tFour\tFive\tSix\tSeven \n\t\t\t\t\t\t\tEight\n" +
-				"\t1One\t2Two\t3Three\t\t5Five\t6Six\t7Seven\n"));
-		}
-
-		[Test]
 		public void WithHeading()
 		{
 			File.WriteAllText(m_TempFile, @"{\rtf1\ansi\ansicpg65001\deff0\deflang1033{" +
@@ -218,11 +195,11 @@ namespace TntMPDConverter
 			Assert.That(converter.Text, Is.EqualTo(
 @"	31953	APerson, A	Street 5	p: 00972/12345	1	40,00
 			72270 Somewhere
-	40462	BPerson, B	Road 43		1	30,00
+	40462	BPerson, B	Road 43	1	30,00
 			71083 Otherwhere
-	40883	CPerson, C	Alley 32		3	-60,00
+	40883	CPerson, C	Alley 32	3	-60,00
 			70734 Nowhere
-	28219	DPerson, D	Main St. 73		1	12,00
+	28219	DPerson, D	Main St. 73	1	12,00
 			12345 Berlin
 				my.email@example.de
 "));
